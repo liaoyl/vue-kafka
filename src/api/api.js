f4ee0c0b4ca38d7ccbf4aee135d4b95bb360dec1
liaoyl <183049416@qq.com>
2018-04-04 10:30:12 +0800

    topicManage update

--- src/api/api.js
+++ src/api/api.js
@@ -5,4 +5,6 @@ let base = ''
 export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data) }
 export const getTopicsListPage = params => { return axios.get(`${base}/kafka/topicsbrief/page`, { params: params }) }
 export const getTopicsBrief = params => { return axios.get(`${base}/kafka/topicsbrief`, { params: params }) }
-export const removeTopic = params => { return axios.delete(`${base}/kafka/topics/remove`, { params: params }) }
+export const removeTopic = params => {
+  return axios.delete(`${base}/kafka/topics/remove`, { params: params })
+}
