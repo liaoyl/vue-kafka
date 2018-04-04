1b7cf52f1f972c0704af3dcae524386e0b572343
mazhongxia <mazhongxia>
2018-04-04 11:58:53 +0800

    broker,consumer静态页面

--- src/views/kafka/consumer/consumerDetailList.vue
+++ src/views/kafka/consumer/consumerDetailList.vue
@@ -3,7 +3,9 @@
     <section>
 
 
-      2222
+      2222222
+
+
       <!--列表-->
       <el-table
         v-loading="listLoading"
@@ -11,9 +13,6 @@
         style="width: 100%">
         <el-table-column label="Topic名称" prop="consumerName"></el-table-column>
         <el-table-column width="250px" label="操作">
-          <template slot-scope="scope">
-            <a href="#/kafka/consumer/consumptionDetail"><el-button size="mini" type="primary">查看消费详情</el-button></a>
-          </template>
         </el-table-column>
       </el-table>
       <el-col :span="24" class="toolbar">
